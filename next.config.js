7ca9ca5a57fbf48c02cb8bac5525381359259b2a
zh00g <gracexz@stanford.edu>
2023-04-27 23:03:35 -0700

    configs

--- next.config.js
+++ next.config.js
@@ -2,5 +2,33 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+const withFonts = require('next-fonts');
+
+const fontAwesomeConfig = {
+  // This ensures that the fonts load properly
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+  /* your config options here */
+})
+
+module.exports = withPlugins([
+  withImages,
+  withFonts,
+  withCSS,
+], fontAwesomeConfig);
+
 
 module.exports = nextConfig
